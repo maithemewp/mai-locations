7211afc967b07f6c602f68e288e8c078c568f12b
JiveDig <jivetheuniversal@gmail.com>
2023-05-30 16:22:35 -0400

    js cleanup and edge-case fixes

--- assets/js/mai-locations-filters.js
+++ assets/js/mai-locations-filters.js
@@ -1,10 +1,11 @@
 window.initMap = function() {
-	const url      = new URL( window.location.href );
-	const searches = document.querySelectorAll( '.mailocations-autocomplete' );
-	const filters  = document.querySelectorAll( '.mailocations-filter' );
-	const clears   = document.querySelectorAll( '.mailocations-filter-clear' );
-	const defaults = maiLocationsVars.defaults;
-	var   params   = maiLocationsVars.params;
+	const url          = new URL( window.location.href );
+	const searches     = document.querySelectorAll( '.mailocations-autocomplete' );
+	const filters      = document.querySelectorAll( '.mailocations-filter' );
+	const clears       = document.querySelectorAll( '.mailocations-filter-clear' );
+	const defaults     = maiLocationsVars.defaults;
+	var   params       = maiLocationsVars.params;
+	var   autoComplete = maiLocationsVars.autoComplete;
 
 	/**
 	 * Refresh the page after adding/removing query strings based
@@ -39,44 +40,61 @@ window.initMap = function() {
 	 * Handle autocomplete fields.
 	 */
 	searches.forEach( input => {
-		// const center        = { lat: 50.064192, lng: -130.605469 };
-		// const defaultBounds = {
-		// 	north: center.lat + 0.1,
-		// 	south: center.lat - 0.1,
-		// 	east: center.lng + 0.1,
-		// 	west: center.lng - 0.1,
-		// };
 		var distance = input.parentElement.parentElement.querySelectorAll( '.mailocations-autocomplete-distance' );
 		var unit     = input.parentElement.parentElement.querySelectorAll( '.mailocations-autocomplete-unit' );
 		var clear    = input.parentElement.querySelectorAll( '.mailocations-autocomplete-clear' )[0];
 
+		// Get elements.
 		distance = 'undefined' !== distance ? distance[0] : '';
 		unit     = 'undefined' !== unit ? unit[0] : '';
 
-		const autocomplete = new google.maps.places.Autocomplete(
-			input,
-			{
-				// bounds: defaultBounds,
-				componentRestrictions: { country: "us" },
-				fields: [ 'geometry', 'name' ],
-				strictBounds: false,
-				// types: ["establishment"],
-			}
-		);
+		// Build autcomplete object.
+		const autocomplete = new google.maps.places.Autocomplete( input, autoComplete );
 
 		/**
 		 * Update url query parameters and refresh the page
 		 * when and address is searched.
 		 */
 		autocomplete.addListener( 'place_changed', function() {
-			var place = autocomplete.getPlace();
-			var lat   = place.geometry.location.lat();
-			var lng   = place.geometry.location.lng();
+			var place    = autocomplete.getPlace();
+			var lat      = place.geometry.location.lat();
+			var lng      = place.geometry.location.lng();
+			var country  = null;
+			var state    = null;
+			var province = null;
+
+			console.log( place.address_components );
+
+			// Get address.
+			if ( place.address_components ) {
+				for ( const component of place.address_components ) {
+					var type = component.types[0];
+
+					if ( 'country' === type ) {
+						country = component.short_name;
+					}
+
+					if ( 'administrative_area_level_1' === type ) {
+						state = component.short_name;
+					}
+				}
+
+				// Maybe set state/province.
+				if ( country ) {
+					if ( 'US' === country ) {
+						if ( state ) {
+							params['state'] = state;
+						}
+					} else if ( province ) {
+						params['province'] = province;
+					}
+				}
+			}
 
 			// Set query params.
-			params[ 'address' ] = input.value;
-			params[ 'lat' ]     = lat;
-			params[ 'lng' ]     = lng;
+			params['address'] = input.value;
+			params['lat']     = lat;
+			params['lng']     = lng;
 
 			// Refresh.
 			refreshPage();
@@ -88,7 +106,7 @@ window.initMap = function() {
 			 * Set distance and only refresh if there is an address.
 			 */
 			distance.addEventListener( 'change', function() {
-				params[ 'distance' ] = this.value;
+				params['distance'] = this.value;
 
 				if ( params['address'] ) {
 					refreshPage();
@@ -101,7 +119,7 @@ window.initMap = function() {
 			 * Set unit and only refresh if there is an address.
 			 */
 			unit.addEventListener( 'change', function() {
-				params[ 'unit' ] = this.value;
+				params['unit'] = this.value;
 
 				if ( params['address'] ) {
 					refreshPage();
@@ -117,10 +135,12 @@ window.initMap = function() {
 
 			// Clear input value and params.
 			input.setAttribute( 'value', '' ); // Empty attribute.
-			input.value = ''; // Empty visual value.
-			params[ 'address' ]  = '';
-			params[ 'lat' ]      = '';
-			params[ 'lng' ]      = '';
+			input.value        = '';  // Empty visual value.
+			params['address']  = '';
+			params['lat']      = '';
+			params['lng']      = '';
+			params['state']    = '';
+			params['province'] = '';
 
 			if ( value ) {
 				refreshPage();
