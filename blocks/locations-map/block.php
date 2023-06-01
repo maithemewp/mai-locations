beb30956f2952f923f503f29d3639eaed358e31c
JiveDig <jivetheuniversal@gmail.com>
2023-06-01 16:27:40 -0400

    allow showing all locations on map by default

--- blocks/locations-map/block.php
+++ blocks/locations-map/block.php
@@ -39,6 +39,7 @@ function mailocations_do_locations_map_block( $attributes, $content, $is_preview
 	echo mailocations_get_stylesheet_link( 'mai-locations' );
 
 	// Values.
+	$get    = get_field( 'query' );
 	$width  = get_field( 'width' );
 	$width  = $width ? absint( $width ) : 800;
 	$height = get_field( 'height' );
@@ -53,23 +54,37 @@ function mailocations_do_locations_map_block( $attributes, $content, $is_preview
 	else {
 		global $wp_query;
 
-		if ( $wp_query ) {
+		// Get all posts.
+		$posts = $wp_query->posts;
+
+		// If showing all.
+		if ( $get && 'all' === $get && ! mailocations_is_filtered_locations() ) {
+			$args             = $wp_query->query;
+			$args['nopaging'] = true;
+
+			unset( $args['posts_per_page'] );
+
+			$all   = new WP_Query( $args );
+			$posts = $all->posts;
+
+			wp_reset_postdata();
+		}
+
+		if ( $posts ) {
 			printf( '<div style="aspect-ratio:%s/%s;" class="mailocations-map" data-zoom="%s">', $width, $height, 7 );
 
-			if ( $wp_query->posts ) {
-				foreach ( $wp_query->posts as $post ) {
-					$lat = get_post_meta( $post->ID, 'location_lat', true );
-					$lng = get_post_meta( $post->ID, 'location_lng', true );
+			foreach ( $posts as $post ) {
+				$lat = get_post_meta( $post->ID, 'location_lat', true );
+				$lng = get_post_meta( $post->ID, 'location_lng', true );
 
-					if ( ! ( $lat && $lng ) ) {
-						continue;
-					}
-
-					printf( '<div style="display:none;" class="marker" data-lat="%s" data-lng="%s">', esc_html( $lat ), esc_html( $lng ) );
-						printf( '<strong><a href="%s">%s</a></strong>', get_permalink( $post->ID ), get_the_title( $post->ID ) );
-						echo mailocations_get_address( [], $post->ID );
-					echo '</div>';
+				if ( ! ( $lat && $lng ) ) {
+					continue;
 				}
+
+				printf( '<div style="display:none;" class="marker" data-lat="%s" data-lng="%s">', esc_html( $lat ), esc_html( $lng ) );
+					printf( '<strong><a href="%s">%s</a></strong>', get_permalink( $post->ID ), get_the_title( $post->ID ) );
+					echo mailocations_get_address( [], $post->ID );
+				echo '</div>';
 			}
 
 			echo '</div>';
@@ -96,9 +111,14 @@ function mailocations_register_locations_map_field_group() {
 			'title'  => __( 'Mai Locations Map', 'mai-locations' ),
 			'fields' => [
 				[
-					'key'          => 'mailocations_map_description',
-					'type'         => 'message',
-					'message'      => __( 'Width/Height mostly used for aspect ratio to prevent CLS.', 'mai-locations' ),
+					'key'          => 'mailocations_map_query',
+					'label'        => __( 'Locations to show by default', 'mai-locations' ),
+					'name'         => 'query',
+					'type'         => 'select',
+					'choices'      => [
+						''    => __( 'Current page', 'mai-locations' ),
+						'all' => __( 'All Locations', 'mai-locations' ),
+					],
 				],
 				[
 					'key'         => 'mailocations_map_width',
@@ -117,6 +137,11 @@ function mailocations_register_locations_map_field_group() {
 					'min'         => 1,
 					'step'        => 1,
 					'placeholder' => 533,
+				],
+				[
+					'key'         => 'mailocations_map_description',
+					'type'        => 'message',
+					'message'     => __( 'Width and height values are used to set the aspect ratio and prevent CLS.', 'mai-locations' ),
 				],
 			],
 			'location' => [
--- mai-locations.php
+++ mai-locations.php
@@ -132,6 +132,7 @@ final class Mai_Locations_Plugin {
 		foreach ( glob( MAI_LOCATIONS_PLUGIN_DIR . 'classes/' . '*.php' ) as $file ) { include $file; }
 		// Blocks.
 		include MAI_LOCATIONS_PLUGIN_DIR . 'blocks/locations-address-search/block.php';
+		include MAI_LOCATIONS_PLUGIN_DIR . 'blocks/locations-count/block.php';
 		include MAI_LOCATIONS_PLUGIN_DIR . 'blocks/locations-filter/block.php';
 		include MAI_LOCATIONS_PLUGIN_DIR . 'blocks/locations-filter-clear/block.php';
 		include MAI_LOCATIONS_PLUGIN_DIR . 'blocks/locations-map/block.php';
