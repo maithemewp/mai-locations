80bd7ed6ec66379cc81055c7c79f861ea865d7ec
JiveDig <jivetheuniversal@gmail.com>
2024-03-21 14:17:26 -0400

    remove embed dependencies

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -6,9 +6,6 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    '7b11c4dc42b3b3023073cb14e519683c' => $vendorDir . '/ralouphie/getallheaders/src/getallheaders.php',
     '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => $vendorDir . '/symfony/polyfill-mbstring/bootstrap.php',
-    'a4a119a56e50fbb293281d9a48007e0e' => $vendorDir . '/symfony/polyfill-php80/bootstrap.php',
-    '09fc349b549513bf7f4291502426f919' => $vendorDir . '/embed/embed/src/functions.php',
     'd05ecc14ff93fd612a81ec7e8ab4c2c9' => $vendorDir . '/yahnis-elsts/plugin-update-checker/load-v5p4.php',
 );
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,10 +7,7 @@ namespace Composer\Autoload;
 class ComposerStaticInit15a83dc919f8db61c7a4325dc93d224e
 {
     public static $files = array (
-        '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
-        'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
-        '09fc349b549513bf7f4291502426f919' => __DIR__ . '/..' . '/embed/embed/src/functions.php',
         'd05ecc14ff93fd612a81ec7e8ab4c2c9' => __DIR__ . '/..' . '/yahnis-elsts/plugin-update-checker/load-v5p4.php',
     );
 
@@ -21,38 +18,11 @@ class ComposerStaticInit15a83dc919f8db61c7a4325dc93d224e
         ),
         'S' => 
         array (
-            'Symfony\\Polyfill\\Php80\\' => 23,
             'Symfony\\Polyfill\\Mbstring\\' => 26,
-            'Slim\\Psr7\\' => 10,
-        ),
-        'P' => 
-        array (
-            'Psr\\Http\\Message\\' => 17,
-            'Psr\\Http\\Client\\' => 16,
-        ),
-        'M' => 
-        array (
-            'ML\\JsonLD\\' => 10,
-        ),
-        'H' => 
-        array (
-            'HtmlParser\\' => 11,
         ),
         'G' => 
         array (
             'Giggsey\\Locale\\' => 15,
-        ),
-        'F' => 
-        array (
-            'Fig\\Http\\Message\\' => 17,
-        ),
-        'E' => 
-        array (
-            'Embed\\' => 6,
-        ),
-        'C' => 
-        array (
-            'Composer\\CaBundle\\' => 18,
         ),
     );
 
@@ -61,70 +31,18 @@ class ComposerStaticInit15a83dc919f8db61c7a4325dc93d224e
         array (
             0 => __DIR__ . '/..' . '/giggsey/libphonenumber-for-php/src',
         ),
-        'Symfony\\Polyfill\\Php80\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/polyfill-php80',
-        ),
         'Symfony\\Polyfill\\Mbstring\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/polyfill-mbstring',
-        ),
-        'Slim\\Psr7\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/slim/psr7/src',
-        ),
-        'Psr\\Http\\Message\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/psr/http-factory/src',
-            1 => __DIR__ . '/..' . '/psr/http-message/src',
-        ),
-        'Psr\\Http\\Client\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/psr/http-client/src',
-        ),
-        'ML\\JsonLD\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/ml/json-ld',
-        ),
-        'HtmlParser\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/oscarotero/html-parser/src',
         ),
         'Giggsey\\Locale\\' => 
         array (
             0 => __DIR__ . '/..' . '/giggsey/locale/src',
         ),
-        'Fig\\Http\\Message\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/fig/http-message-util/src',
-        ),
-        'Embed\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/embed/embed/src',
-        ),
-        'Composer\\CaBundle\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/composer/ca-bundle/src',
-        ),
-    );
-
-    public static $prefixesPsr0 = array (
-        'M' => 
-        array (
-            'ML\\IRI' => 
-            array (
-                0 => __DIR__ . '/..' . '/ml/iri',
-            ),
-        ),
     );
 
     public static $classMap = array (
-        'Attribute' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Attribute.php',
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
-        'PhpToken' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/PhpToken.php',
-        'Stringable' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Stringable.php',
-        'UnhandledMatchError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/UnhandledMatchError.php',
-        'ValueError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/ValueError.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
@@ -132,7 +50,6 @@ class ComposerStaticInit15a83dc919f8db61c7a4325dc93d224e
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit15a83dc919f8db61c7a4325dc93d224e::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit15a83dc919f8db61c7a4325dc93d224e::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInit15a83dc919f8db61c7a4325dc93d224e::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit15a83dc919f8db61c7a4325dc93d224e::$classMap;
 
         }, null, ClassLoader::class);
